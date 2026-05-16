4f4211e67debf0062916f9e97805f8aa6ded5164
RedHammer221 <simeon.g.vasilev@gmail.com>
2026-05-16 12:34:38 +0300

    Added Mapping for getting all carts and a particular user's cart

--- src/main/java/com/ecommerce/project/controller/CartController.java
+++ src/main/java/com/ecommerce/project/controller/CartController.java
@@ -1,16 +1,27 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.model.Cart;
 import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.repositories.CartRepository;
 import com.ecommerce.project.service.CartService;
+import com.ecommerce.project.util.AuthUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 
 public class CartController {
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    AuthUtil authUtil;
 
     @Autowired
     CartService cartService;
@@ -20,5 +31,20 @@ public class CartController {
                                                     @PathVariable Integer quantity) {
         CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
         return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/carts")
+    public ResponseEntity<List<CartDTO>> getCarts() {
+        List<CartDTO> cartDTOs = cartService.getAllCarts();
+        return new ResponseEntity<List<CartDTO>>(cartDTOs, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/carts/users/cart")
+    public ResponseEntity<CartDTO> getCartById() {
+        String emailId = authUtil.loggedInEmail();
+        Cart cart = cartRepository.findCartByEmail(emailId);
+        Long cartId = cart.getCartId();
+        CartDTO cartDTO = cartService.getCart(emailId, cartId);
+        return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/project/service/CartService.java
+++ src/main/java/com/ecommerce/project/service/CartService.java
@@ -4,7 +4,13 @@ import com.ecommerce.project.payload.CartDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Service
 public interface CartService {
     CartDTO addProductToCart(Long productId, Integer quantity);
+
+    List<CartDTO> getAllCarts();
+
+    CartDTO getCart(String emailId, Long cartId);
 }
