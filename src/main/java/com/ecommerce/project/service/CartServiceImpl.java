f0d62296e90c1b6023866191b45096cf7f8c0e85
RedHammer221 <simeon.g.vasilev@gmail.com>
2026-05-16 17:47:51 +0300

    Tested and fixed errors

--- src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -156,18 +156,29 @@ public class CartServiceImpl implements CartService {
 
         CartItem cartItem = cartItemRepository.findCartItemByProductIdAndCartId(cartId, productId);
         if(cartItem == null) {
-            throw new APIException("Cart " + product.getProductName() + " not in the cart");
+            throw new APIException("Product " + product.getProductName() + " not in the cart");
         }
 
-        cartItem.setProductPrice(product.getSpecialPrice());
-        cartItem.setQuantity(cartItem.getQuantity() + quantity);
-        cartItem.setDiscount(product.getDiscount());
-        cart.setTotalPrice(cart.getTotalPrice() + (cartItem.getProductPrice() * quantity));
-        cartRepository.save(cart);
-        CartItem updatedItem = cartItemRepository.save(cartItem);
-        if(updatedItem.getQuantity() == 0) {
-            cartItemRepository.deleteById(updatedItem.getCartItemId());
+        int newQuantity = cartItem.getQuantity() + quantity;
+
+        if(newQuantity < 0) {
+            throw new APIException("New quantity cannot be negative");
         }
+
+        if(newQuantity == 0) {
+            deleteProductFromCart(cartId, productId);
+        } else {
+            cartItem.setProductPrice(product.getSpecialPrice());
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+            cartItem.setDiscount(product.getDiscount());
+            cart.setTotalPrice(cart.getTotalPrice() + (cartItem.getProductPrice() * quantity));
+            cartRepository.save(cart);
+
+        }
+            CartItem updatedItem = cartItemRepository.save(cartItem);
+            if(updatedItem.getQuantity() == 0) {
+                cartItemRepository.deleteById(updatedItem.getCartItemId());
+            }
 
         CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
         List<CartItem> cartItems = cart.getCartItems();
@@ -182,6 +193,7 @@ public class CartServiceImpl implements CartService {
         return cartDTO;
     }
 
+    @Transactional
     @Override
     public String deleteProductFromCart(Long cartId, Long productId) {
         Cart cart = cartRepository.findById(cartId)
