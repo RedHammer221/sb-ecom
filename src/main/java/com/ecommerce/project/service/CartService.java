e63a16dffe211f0380ebc922a2631c4034029efb
RedHammer221 <simeon.g.vasilev@gmail.com>
2026-05-16 11:37:55 +0300

    Implemented AuthUtils class

--- src/main/java/com/ecommerce/project/service/CartService.java
+++ src/main/java/com/ecommerce/project/service/CartService.java
@@ -1,8 +1,10 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.payload.CartDTO;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Service
 public interface CartService {
     CartDTO addProductToCart(Long productId, Integer quantity);
 }
