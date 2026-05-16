3cf508efa643f75a1ba6d0065659599c98bd0fd9
RedHammer221 <simeon.g.vasilev@gmail.com>
2026-05-16 17:24:29 +0300

    Added API endpoint, service and implementation for deleting cart items

--- src/main/java/com/ecommerce/project/controller/CartController.java
+++ src/main/java/com/ecommerce/project/controller/CartController.java
@@ -48,12 +48,19 @@ public class CartController {
         return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
     }
 
-    @PutMapping("/cart/products/{priductId}/quantity/{operation}")
+    @PutMapping("/cart/products/{productId}/quantity/{operation}")
     public ResponseEntity<CartDTO> updateCartProduct(@PathVariable Long productId,
                                                      @PathVariable String operation) {
         CartDTO cartDTO = cartService.updateProductQuantityInCart(productId,
                 operation.equalsIgnoreCase("delete") ? -1 : 1);
 
         return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/carts/{cartId}/product/{productId}")
+    public ResponseEntity<String> deleteCartProduct(@PathVariable Long cartId, @PathVariable Long productId) {
+        cartService.deleteProductFromCart(cartId, productId);
+        String status = cartService.deleteProductFromCart(cartId, productId);
+        return new ResponseEntity<String>(status, HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/project/repositories/CartRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartRepository.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
