0d7c97509d52edf3831d0b51ba9a4430d2553642
RedHammer221 <simeon.g.vasilev@gmail.com>
2026-06-06 14:08:18 +0300

    Added entities for orders and order items

---
+++ src/main/java/com/ecommerce/project/model/OrderItem.java
@@ -0,0 +1,28 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "order_items")
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne
+    private Product product;
+
+    @ManyToOne
+    private Order order;
+
+
+    private Integer quantity;
+    private double discount;
+    private double orderedProductPrice;
+}
