a0d0655ca795490d880b28aadb8f8f35ec73f0f1
RedHammer221 <simeon.g.vasilev@gmail.com>
2026-08-22 15:34:22 +0300

    Added Swagger API documentation

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -4,6 +4,12 @@ import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,35 +23,59 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
+    @Operation(summary = "Get all categories")
     @GetMapping("/public/categories")
 //    @RequestMapping(value = "/public/categories", method = RequestMethod.GET)
     public ResponseEntity<CategoryResponse> getAllCategories(
-            @RequestParam (name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER,
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER,
                     required = false) Integer pageNumber,
             @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE,
-                    required = false)Integer pageSize,
-            @RequestParam (name = "sortBy", defaultValue = AppConstants.SORT_CATEGORIES_BY) String sortBy,
+                    required = false) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_CATEGORIES_BY) String sortBy,
             @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR) String sortOrder) {
         CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize, sortBy, sortOrder);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
-//    @RequestMapping(value = "/public/categories", method = RequestMethod.POST)
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
+    @Operation(summary = "Create categories", description = "API to create a new category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "category created successfully"),
+            @ApiResponse(responseCode = "400", description = "invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @PostMapping("/public/categories")
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
-       CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
+        CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
         return new ResponseEntity<>(savedCategoryDTO, HttpStatus.CREATED);
     }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
+    @Operation(summary = "Delete categories", description = "API to delete a category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "category deleted successfully"),
+            @ApiResponse(responseCode = "400", description = "invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId) {
-            CategoryDTO deletedCategory = categoryService.deleteCategory(categoryId);
-            return new ResponseEntity<>(deletedCategory, HttpStatus.OK);
+    public ResponseEntity<CategoryDTO> deleteCategory(@Parameter(description = "ID of the category that you wish to delete")
+                                                      @PathVariable Long categoryId) {
+        CategoryDTO deletedCategory = categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(deletedCategory, HttpStatus.OK);
     }
 
+    @Tag(name = "Category APIs", description = "APIs for managing categories")
+    @Operation(summary = "Update categories", description = "API to update a category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "category updated successfully"),
+            @ApiResponse(responseCode = "400", description = "invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @PutMapping("/public/categories/{categoryId}")
-    public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO, @PathVariable Long categoryId) {
-            CategoryDTO savedCategoryDTO = categoryService.updateCategory(categoryDTO, categoryId);
-            return new ResponseEntity<>(savedCategoryDTO, HttpStatus.OK);
+    public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO,
+                                                      @Parameter(description = "ID of the category that you wish to update") @PathVariable Long categoryId) {
+        CategoryDTO savedCategoryDTO = categoryService.updateCategory(categoryDTO, categoryId);
+        return new ResponseEntity<>(savedCategoryDTO, HttpStatus.OK);
     }
 }
